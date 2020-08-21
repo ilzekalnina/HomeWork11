89588059c6b1417ff3db05ec7d63231ca8f2941d
Ilze Kalnina <ilze@skate.lv>
2020-08-21 17:31:33 +0300

    Commit 1

--- HomeWork11/ViewController.swift
+++ HomeWork11/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redButton: UIButton!
+    
+    @IBOutlet weak var yellowButton: UIButton!
+    
+    @IBOutlet weak var greenButton: UIButton!
+    
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var isRed = false
+    var isYellow = false
+    var isGreen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextTapped(_ sender: Any) {
+        if !isRed && !isYellow && !isGreen {
+            isRed = true
+            self.redButton.backgroundColor = UIColor.red
+        }
+        if isRed {
+            isRed = false
+            self.redButton.backgroundColor = UIColor.gray
+            self.yellowButton.backgroundColor = UIColor.yellow
+            isYellow = true
+        } else {
+            if isYellow {
+                isYellow = false
+                self.yellowButton.backgroundColor = UIColor.gray
+                self.greenButton.backgroundColor = UIColor.green
+                isGreen = true
+            } else{
+                isGreen = false
+                self.greenButton.backgroundColor = UIColor.gray
+                self.redButton.backgroundColor = UIColor.red
+                isRed = true
+            }
+        }
+    }
+    
 }
 
